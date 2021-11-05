8c56fd271a8f550703cb86eff5f5902178c112e8
Magnus Forslin <magnus.forslin@gmail.com>
2021-11-05 13:08:47 +0100

    Add project files.

---
+++ deletty/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="deletty.MvcApplication" Language="C#" %>
---
+++ deletty/Models/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace deletty.Models
+{
+    public class TheClass
+    {
+        public int Thing { get; set; }
+    }
+}
